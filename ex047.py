cd7dddb39ad388ce7714d12cad3d190db9e79390
Danielns <danielnoronha.sh@gmail.com>
2020-12-01 23:15:12 -0300

    Todos os arquivos foram removidos para correção

--- ex047.py
+++
@@ -1,6 +0,0 @@
-print('Números pares de 1 a 50:')
-
-for c in range(1, 51):
-    if c % 2 == 0:
-        print(c, end=' ')
-print('Fim!')
--- ex063.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Informe um número: '))
-cont = 1
-termA, termB = 0, 1
-soma = 0
-print('{} -> {}'.format(termA, termB), end='')
-while cont < num:
-    termA, termB = termB, termB + termA
-    print(' -> {}'.format(termB), end='')
-    cont += 1
