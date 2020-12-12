8a799b98ce82312abbcabce05392c66ccdce05c8
Danielns <danielnoronha.sh@gmail.com>
2020-12-12 08:22:05 -0300

    Alteração de lógica para pegar os dados da lista

---
+++ CursoEmVideo/exercicios/ex095.py
@@ -0,0 +1,62 @@
+# Aprimore o DESAFIO 093 para que ele funcione com vários
+# jogadores, incluindo um sistema de visualização de detalhes
+# do aproveitamento de cada jogador.
+from time import sleep
+
+db_jogadores = {}
+
+print(f'{"Análise Tática":-^40}')
+cont = 1
+while True:
+    print('-' * 40)
+    dados_jogador = {'Nome': str(input('Nome do jogador: ')).strip(),
+                     'Partidas': int(input('Partidas jogadas: ')),
+                     'Gols': [],
+                     'Total de Gols': 0}
+
+    for p in range(1, dados_jogador['Partidas'] + 1):
+        gols = int(input(f'O Jogador {dados_jogador["Nome"]} fez quantos gols na {p}ª partida: '))
+        dados_jogador['Gols'].append(gols)
+        dados_jogador['Total de Gols'] += gols
+
+    db_jogadores[f'Jogador_{cont}'] = dados_jogador.copy()
+    dados_jogador.clear()
+    continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+    while continuar not in 'SN':
+        continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+    if continuar == 'N':
+        print('-' * 40)
+        break
+    else:
+        cont += 1
+
+print(db_jogadores)
+while True:
+    print('-' * 40)
+    print(f'{"COD":<5}{"Nome":<10}{"Gols":<15}{"Total":<5}')
+    cod = 0
+    while cod < len(db_jogadores):
+        cod += 1
+        for chave, valor in db_jogadores.items():
+            for k, v in valor.items():
+                print(v)
+            #print(f'{cod:>5} {valores["Nome"]}{valores["Gols"]}{valores["Total de Gols"]}')
+    print('-' * 40)
+    visualizar = int(input('Visualizar dados de qual jogador [COD]: '))
+    while visualizar > len(db_jogadores):
+        print(f'Favor informar um número entre 1 e {len(db_jogadores)}.')
+        visualizar = int(input('Visualizar dados de qual jogador [COD]: '))
+
+print('-' * 40)
+#sleep(1)
+
+#for k, v in dados_jogador.items():
+#    print(f'{k}: {v}')
+#print('-' * 40)
+#sleep(1)
+
+#print(f'O Jogador {dados_jogador["Nome"]} jogou {dados_jogador["Partidas"]} partidas.')
+#for pos, quant in enumerate(dados_jogador['Gols']):
+#    print(f'\t=> Na partida {pos + 1}, {dados_jogador["Nome"]} fez {quant} gols.')
+#print(f'Foi um total de {dados_jogador["Total de Gols"]} gols.')
+#print('-' * 40)
