06c7d25a577144e6fcbb1896476691e2d39e103f
Danielns <danielnoronha.sh@gmail.com>
2021-01-26 06:10:20 -0300

    v1.2 - correção do BUG das vogais

--- Pyforca/motor.py
+++ Pyforca/motor.py
@@ -33,9 +33,8 @@ def escolhe_nivel_palavra():
     return escolha
            
             
-def valida_vogal(supor, palavra_escolhida, vidas=vida, letra_supor=erro):
+def valida_vogal(supor):
     lista_vogal = []
-    validar = False
 
     if supor == 'a':
         lista_vogal = palavras.vogal_a
@@ -47,17 +46,9 @@ def valida_vogal(supor, palavra_escolhida, vidas=vida, letra_supor=erro):
         lista_vogal = palavras.vogal_o
     elif supor == 'u':
         lista_vogal = palavras.vogal_u
-    
-    for posicao in range(len(palavra_escolhida)):
-        for vogal in lista_vogal:
-            if vogal == palavra_escolhida[posicao]:
-                mostrar[posicao] = vogal
-                validar = True
 
-    if not validar:
-        letra_supor += supor
-        vidas -= 1
-            
+    return lista_vogal
+
 
 def execucao(nome, vidas=vida, letra_supor=erro):
     palavra = nome
@@ -66,29 +57,48 @@ def execucao(nome, vidas=vida, letra_supor=erro):
         mostrar.append('_')
 
     while True:
+        # váriavel para validar se existe não existe vogais com acentos.
+        existe = 0
+        
         print(artes.estagio[vidas])
         print(f'Palavra: {" ".join(mostrar)}')
         print(f'Letras erradas: {"-".join(letra_supor)}')
         print('-=' * 30)
+
+        if "_" not in mostrar:
+            print('Parabéns!!! Você venceu!')
+            break
+
         supor = str(input('Adivinhe uma letra: ')).strip().lower()
+
         if len(supor) != 1:
             print('Atenção! Informe apenas uma letra.')
         elif supor.isnumeric():
             print('[ERRO] - Informe apenas letra.')
         else:
+            # verifica se existe a vogal com acentos
             if supor in 'aeiou':
-                valida_vogal(supor, palavra)
+                grupo_vogal = valida_vogal(supor)
+                for posicao in range(len(palavra)):
+                    for vogal in grupo_vogal:
+                        # verifica se cada vogal do grupo está na palavra,
+                        # Ex: vogal 'e' grupo: ['e', 'é', 'è', 'ê']
+                        if vogal == palavra[posicao]:
+                            # caso exista uma vogal acentuada, a mesma será mostrada.
+                            mostrar[posicao] = vogal
+                            existe += 1
+                # caso nenhuma volgal do grupo exista é necessário reduzir a vida
+                # e mostrar a vogal errada.
+                if existe == 0:
+                    letra_supor += supor
+                    vidas -= 1
             else:
-                for index in range(len(palavra)):
-                    if supor == palavra[index]:
-                        mostrar[index] = supor
+                for posicao in range(len(palavra)):
+                    if supor == palavra[posicao]:
+                        mostrar[posicao] = supor
                 if supor not in palavra:
                     letra_supor += supor
                     vidas -= 1
                 if vidas < 0:
                     print(f'Você perdeu, a palavra é {palavra}')
                     break
-                if "_" not in mostrar:
-                    print('Parabéns!!! Você venceu!')
-                    break
-
