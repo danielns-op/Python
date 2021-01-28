25cd4c9a9de10e3d87c9f437bd2a6a1ad6ef2321
Danielns <danielnoronha.sh@gmail.com>
2021-01-28 14:34:16 -0300

    fix some bugs, stable version.

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,8 +1,9 @@
 import logo
-from random import choice
+from random import choice, randint
 from time import sleep
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 
 def presentation():
     will_play = True
@@ -53,64 +54,109 @@ def get_another_card(var):
 def play():
     playing = presentation()
     balance = [1000]
-    if playing:
-        print('-=' * 32)
-        while True:
-            try:
-                bet_player = int(input('->How much you want to bet: $'))
-                if bet_player < 1 or bet_player > balance[0]:
-                    print(f'Attention, enter value between 1 and {balance[0]}.')
+    while playing:
+        if playing:
+            print('-=' * 32)
+            while True:
+                try:
+                    print(f'>Balance: {balance[0]}')
+                    bet_player = int(input('->How much you want to bet: $'))
+                    if bet_player < 1 or bet_player > balance[0]:
+                        print(f'Attention, enter value between 1 and {balance[0]}.')
+                        sleep(2)
+                    else:
+                        break
+                except ValueError:
+                    print('[ERROR] - Integer values only.')
                     sleep(2)
-                else:
-                    break
-            except ValueError:
-                print('[ERROR] - Integer values only.')
-                sleep(2)
-        print('-=' * 32)
-        player_cards = [choose_card(), choose_card()]
-        balance_player = calculate_balance(balance, bet_player)
-        player_score = sum_cards(player_cards)
-        print(f'>Your cards: {player_cards}')
-        print(f'>Current score: {player_score}')
-        print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
+            print('-=' * 32)
+            player_cards = [choose_card(), choose_card()]
+            balance_player = calculate_balance(balance, bet_player)
+            player_score = sum_cards(player_cards)
+            balance[0] = balance_player
+            print(f'>Your cards: {player_cards}')
+            print(f'>Current score: {player_score}')
+            print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
 
-        print('-=' * 16)
+            print('-=' * 16)
 
-        bot_cards = [choose_card(), choose_card()]
-        bot_score = sum_cards(bot_cards)
-        print(f'{f">Bot first card: {bot_cards[0]}":>50}')
-        print('')
+            bot_cards = [choose_card(), choose_card()]
+            bot_score = sum_cards(bot_cards)
+            print(f'{f">Bot first card: {bot_cards[0]}":>50}')
+            print('-=' * 32)
 
-        while True:
-            get_card = str(input('Take another card? [Y / n]: ')).strip().lower()
-            if get_card == 'y':
-                get_another_card(player_cards)
-                player_score = sum_cards(player_cards)
-                if player_score > 20:
-                    break
+            while True:
+                get_card = '0'
+                # get_card = str(input('Take another card? [Y / n]: ')).strip().lower()
+                while get_card not in 'yn':
+                    get_card = str(input('Take another card? [Y / n]: ')).strip().lower()
+                if get_card == 'y':
+                    get_another_card(player_cards)
+                    player_score = sum_cards(player_cards)
+                    if player_score < 21:
+                        print('-=' * 32)
+                        print(f'>Your cards: {player_cards}')
+                        print(f'>Current score: {player_score}')
+                        print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
+                    elif player_score == 21:
+                        break
+                    else:
+                        print('-=' * 32)
+                        print(f'>Your cards: {player_cards}')
+                        print(f'>Current score: {player_score}')
+                        print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
+                        break
                 else:
                     player_score = sum_cards(player_cards)
                     print('-=' * 32)
-                    print(f'>Your cards: {player_cards}')
-                    print(f'>Current score: {player_score}')
+                    print(f'>Your final cards: {player_cards}')
+                    print(f'>Your final score: {player_score}')
                     print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
-            else:
-                player_score = sum_cards(player_cards)
-                print('-=' * 32)
-                print(f'>Your final cards: {player_cards}')
-                print(f'>Your final score: {player_score}')
-                print(f'>Bet: ${bet_player}  --  Balance: ${balance_player}')
 
-                while bot_score < 21:
-                    get_another_card(bot_cards)
-                    bot_score = sum_cards(bot_cards)
-                print('-=' * 16)
-                print(f'{f">Bot final hand: {bot_cards}":>58}')
-                print(f'{f">Bot final score: {bot_score}":>50}')
-                print('')
-    else:
-        print(f'{"Leaving the game.":^64}')
-        sleep(1.5)
+                    while True:
+                        if 15 < bot_score <= 17:
+                            luck = randint(1, 10)
+                            if luck >= 6:
+                                get_another_card(bot_cards)
+                                bot_score = sum_cards(bot_cards)
+                        elif bot_score <= 14:
+                            get_another_card(bot_cards)
+                            bot_score = sum_cards(bot_cards)
+                        elif bot_score <= 21:
+                            break
+                        else:
+                            break
+                    break
+            print('-=' * 16)
+            print(f'{f">Bot final hand: {bot_cards}":>58}')
+            print(f'{f">Bot final score: {bot_score}":>50}')
+            print('-=' * 32)
+
+            if player_score == bot_score:
+                print(f'{"*** A tie ! ***":^64}')
+                balance_player += bet_player
+            elif player_score <= 21 and bot_score > 21 or 21 >= player_score > bot_score:
+                print(f'{"|---- You Win !!! ----|":64}')
+                balance_player = balance_player + (bet_player * 2)
+                balance[0] = balance_player
+                print(f'{f"--> Balance: ${balance_player}":^60}')
+            elif bot_score <= 21 and player_score > 21 or player_score < bot_score:
+                print(f'{"~~~~ You Lose. ~~~~":^64}')
+                print(f'{f"--> Balance: ${balance_player}":^60}')
+                if balance_player <= 0:
+                    print('No balance, game over for you.')
+                    sleep(2)
+                    exit(0)
+        else:
+            print(f'{"Leaving the game.":^64}')
+            playing = False
+            sleep(1.5)
+        again = str(input('Play again? [Y / n]: ')).strip().lower()
+        if again == 'y':
+            continue
+        else:
+            break
 
 
 play()
+print('See you next time!')
