829441c1265f3023deef2b01ceacfce801f5f306
Danielns <danielnoronha.sh@gmail.com>
2021-01-28 02:14:31 -0300

    Adicionado cabeçalho e informações v1.3

--- Pyforca/pyforca.py
+++ Pyforca/pyforca.py
@@ -1,3 +1,21 @@
+# pyforca.py #################################################### #
+#                                                                 #
+# Joguinha da forca para se divertir um pouco.                    #
+#                                                                 #
+#  Autor: Daniel Noronha                                          #
+#  Email: danielnoronha.sh@gmail.com                              #
+# GitHub: https://github.com/danielns-op/Python/tree/main/Pyforca #
+# --------------------------------------------------------------- #
+# Utilização:                                                     #
+#   Cópia a pasta Pyforca que se encontra no repositório          #
+#   acima e execute o arquivo pyforca.py.                         #
+#       Ex: python3 pyforca.py                                    #
+#                                                                 #
+# Caso queira adicionar mais palavras, adicione na lista do       #
+# arquivo palavras.py de acordo com a quantidade de letras.       #
+#                                                                 #
+# v1.3                                                            #
+# ############################################################### #
 import artes
 import motor
 
