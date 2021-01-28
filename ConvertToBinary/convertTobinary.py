edfe49f89b70b0571a6c8add20595a0e3d1a18f9
Danielns <danielnoronha.sh@gmail.com>
2021-01-28 07:55:47 -0300

    converts string em binary

---
+++ ConvertToBinary/convertTobinary.py
@@ -0,0 +1,19 @@
+# convertTobinary ################################################ #
+#
+# Author: Daniel Noronha
+#  Email: danielnoronha.sh@gmail.com
+# GitHub:
+#                                                         #
+# ####################################################### #
+
+
+def convert_binary(value):
+    """
+    Converts any value entered in the value parameter to binary,
+    if it is INT or FLOAT it will be converted to STRING.
+    :param value: Receive the amount that will be converted to
+    STRING and transformed into binary.
+    :return: Returns the value in binary.
+    """
+    binary = ''.join(format(char, 'b') for char in bytearray(str(value), 'utf-8'))
+    return binary
