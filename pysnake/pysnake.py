282ed54508b22dbffacb8a18070bf23496b72e71
Danielns <danielnoronha.sh@gmail.com>
2021-02-06 06:24:18 -0300

    The program was organized and the snake module was imported.

--- pysnake/pysnake.py
+++ pysnake/pysnake.py
@@ -6,47 +6,38 @@
 #  Email: danielnoronha.sh@gmail.com                            #
 # GitHub:
 # ############################################################# #
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 from time import sleep
-from move_snake import Snake
 
 # Screen configuration.
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('DarkGray')
 screen.title('pySnake Game')
+# The tracer() function works in conjunction with the
+# update() function in the game loop.
+screen.tracer(0)
 
 # Snake characteristics
-starting_positions = [(0, 0), (-11, 0), (-22, 0)]
-snake_body = []
-
-# Defining the movement parameters to be used in the snake class.
-move_snake = Snake(snake=snake_body)
-
-for position in starting_positions:
-    new_snake_fragment = Turtle(shape='square')
-    new_snake_fragment.shapesize(stretch_wid=0.5)
-    new_snake_fragment.fillcolor('CornflowerBlue')
-    new_snake_fragment.penup()
-    new_snake_fragment.goto(position)
-    snake_body.append(new_snake_fragment)
+# Import from Snake class
+snake = Snake()
 
 # Execution
 play_game = True
-snake_body[0].fillcolor('blue')
 while play_game:
-    # It is necessary to use update () and sleep () to mount the
+    # It is necessary to use update() and sleep() to mount the
     # image of the snake in motion and not appear with the segmented body.
     screen.update()
     sleep(0.1)
-    move_snake.move_forward()
-    # for segments in snake_body:
-    #     segments.forward(9)
+
+    snake.move_forward()
+
     screen.listen()
-    screen.onkey(fun=move_snake.move_up, key='w')
-    screen.onkey(fun=move_snake.move_left, key='a')
-    screen.onkey(fun=move_snake.move_right, key='d')
-    screen.onkey(fun=move_snake.move_down, key='s')
+    screen.onkey(fun=snake.move_up, key='w')
+    screen.onkey(fun=snake.move_left, key='a')
+    screen.onkey(fun=snake.move_right, key='d')
+    screen.onkey(fun=snake.move_down, key='s')
 
 # Loop up.
 screen.exitonclick()
