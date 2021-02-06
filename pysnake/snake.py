896819100531a97371d197d825aa4f4e4bb56cb8
Danielns <danielnoronha.sh@gmail.com>
2021-02-06 07:52:59 -0300

    Removing unnecessary calculations and loops.

--- pysnake/snake.py
+++ pysnake/snake.py
@@ -9,6 +9,7 @@ class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.snake_head = self.snake[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -24,36 +25,16 @@ class Snake:
             pos_x = self.snake[seg - 1].xcor()
             pos_y = self.snake[seg - 1].ycor()
             self.snake[seg].goto(pos_x, pos_y)
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.snake_head.forward(MOVE_DISTANCE)
 
     def move_up(self):
-        pos_x = self.snake[0].xcor()
-        pos_y = self.snake[0].ycor()
-        self.snake[0].setheading(90)
-        for seg in range(1, len(self.snake)):
-            if self.snake[seg].xcor() == pos_x and self.snake[seg].ycor() == pos_y:
-                self.snake[seg].setheading(90)
+        self.snake_head.setheading(90)
 
     def move_left(self):
-        pos_x = self.snake[0].xcor()
-        pos_y = self.snake[0].ycor()
-        self.snake[0].setheading(180)
-        for seg in range(1, len(self.snake)):
-            if self.snake[seg].xcor() == pos_x and self.snake[seg].ycor() == pos_y:
-                self.snake[seg].setheading(180)
+        self.snake_head.setheading(180)
 
     def move_right(self):
-        pos_x = self.snake[0].xcor()
-        pos_y = self.snake[0].ycor()
-        self.snake[0].setheading(0)
-        for seg in range(1, len(self.snake)):
-            if self.snake[seg].xcor() == pos_x and self.snake[seg].ycor() == pos_y:
-                self.snake[seg].setheading(0)
+        self.snake_head.setheading(0)
 
     def move_down(self):
-        pos_x = self.snake[0].xcor()
-        pos_y = self.snake[0].ycor()
-        self.snake[0].setheading(270)
-        for seg in range(1, len(self.snake)):
-            if self.snake[seg].xcor() == pos_x and self.snake[seg].ycor() == pos_y:
-                self.snake[seg].setheading(270)
+        self.snake_head.setheading(270)
