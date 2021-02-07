2a2ac0c373dd6fffecece00fcafe66c77f701b07
Danielns <danielnoronha.sh@gmail.com>
2021-02-07 16:38:43 -0300

    add new class in main file

--- pysnake/pysnake.py
+++ pysnake/pysnake.py
@@ -9,6 +9,8 @@
 from turtle import Screen
 from snake import Snake
 from time import sleep
+from food import Food
+from score_board import Score
 
 # Screen configuration.
 screen = Screen()
@@ -22,6 +24,18 @@ screen.tracer(0)
 # Snake characteristics
 # Import from Snake class
 snake = Snake()
+apple = Food()
+scoreboard = Score()
+
+screen.listen()
+screen.onkeypress(fun=snake.move_up, key='w')
+screen.onkeypress(fun=snake.move_up, key='Up')
+screen.onkeypress(fun=snake.move_left, key='a')
+screen.onkeypress(fun=snake.move_left, key='Left')
+screen.onkeypress(fun=snake.move_right, key='d')
+screen.onkeypress(fun=snake.move_right, key='Right')
+screen.onkeypress(fun=snake.move_down, key='s')
+screen.onkeypress(fun=snake.move_down, key='Down')
 
 # Execution
 play_game = True
@@ -30,16 +44,16 @@ while play_game:
     # image of the snake in motion and not appear with the segmented body.
     screen.update()
     sleep(0.1)
-
     snake.move_forward()
 
-    screen.onkeypress(fun=snake.move_up, key='w')
-    screen.onkeypress(fun=snake.move_up, key='Up')
-    screen.onkeypress(fun=snake.move_left, key='a')
-    screen.onkeypress(fun=snake.move_left, key='Left')
-    screen.onkeypress(fun=snake.move_right, key='d')
-    screen.onkeypress(fun=snake.move_right, key='Right')
-    screen.onkeypress(fun=snake.move_down, key='s')
-    screen.onkeypress(fun=snake.move_down, key='Down')
-    screen.listen()
-    # Loop up.
+    # Detect collision with apple.
+    if snake.snake_head.distance(apple) < 10:
+        apple.refresh()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.snake_head.xcor() > 280 or snake.snake_head.xcor() < -280 or snake.snake_head.ycor() > 280 or snake.snake_head.ycor() < -280:
+        play_game = False
+
+scoreboard.game_over()
+screen.exitonclick()
