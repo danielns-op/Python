b4c9b9df8838fc6e8bdf9631bc7e79bd184ef9e0
Danielns <danielnoronha.sh@gmail.com>
2021-02-09 03:34:57 -0300

    add Score class

--- ponggame/screen_map.py
+++ ponggame/screen_map.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGNMENT = 'center'
+FONT = ('Arial', 25, 'normal')
+
 
 class LineMiddle:
     def __init__(self):
@@ -17,3 +20,39 @@ class LineMiddle:
         new_seg.penup()
         new_seg.goto(x=0, y=position)
         self.middle_line.append(new_seg)
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('gray0')
+        self.penup()
+        self.hideturtle()
+
+    def update_score(self):
+        self.write(arg=f'{self.score}', align=ALIGNMENT, font=FONT)
+
+    def score_player(self):
+        self.goto(-100, 210)
+        self.update_score()
+
+    def score_bot(self):
+        self.goto(100, 210)
+        self.update_score()
+
+    def winner_game(self):
+        self.color('orange')
+        self.goto(0, 0)
+        self.write(arg='You Won!!!', align=ALIGNMENT, font=('Arial', 50, 'bold'))
+
+    def game_over(self):
+        self.color('red')
+        self.goto(0, 0)
+        self.write(arg='Game Over\nYou lose.', align=ALIGNMENT, font=('Arial', 50, 'bold'))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
