15c3f944be590bd5bd77fd527a6a8580b3107feb
Danielns <danielnoronha.sh@gmail.com>
2021-02-10 14:49:26 -0300

    add level_up function

--- pyCrossyRoad/car.py
+++ pyCrossyRoad/car.py
@@ -3,27 +3,34 @@ from random import choice, randint
 from time import sleep
 
 CARS = ['blue_car.gif', 'grey_car.gif', 'jeep_green.gif', 'red_car.gif', 'yellow_car.gif']
-POS_X = [440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540]
+POS_X = [400, 420, 440, 460, 480, 500]
 POS_Y = [173, 215, 8, -33, -170, -213]
 STARTING_MOVE_DISTANCE = 5
 MOVE_ADD = 10
 
-for car in CARS:
-    Screen().register_shape(car)
+for stile in CARS:
+    Screen().register_shape(stile)
 
 
 class Car:
 
     def __init__(self):
-        self.car = Turtle()
-        self.car.penup()
-        self.car.setheading(180)
-        self.car.shape(choice(CARS))
-        self.pos = (choice(POS_X), choice(POS_Y))
-        self.car.goto(self.pos)
-        self.pos_x = self.car.xcor()
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def move(self):
-        self.pos_x -= 1
-        self.car.goto(self.pos_x, self.car.ycor())
+        for car in self.all_cars:
+            car.backward(self.car_speed)
 
+    def create_car(self):
+        # Car generation control, it will only be generated if the number 15 is drawn.
+        chance = randint(1, 15)
+        if chance == 6:
+            new_car = Turtle(shape=(choice(CARS)))
+            new_car.shapesize(stretch_wid=4, stretch_len=1)
+            new_car.penup()
+            new_car.goto(choice(POS_X), choice(POS_Y))
+            self.all_cars.append(new_car)
+
+    def level_up(self):
+        self.car_speed += MOVE_ADD
