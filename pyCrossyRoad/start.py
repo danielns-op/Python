864188477cae22bf1bb8a539af4a86ac0a98365e
Danielns <danielnoronha.sh@gmail.com>
2021-02-10 15:55:28 -0300

    beta 2 version

--- pyCrossyRoad/start.py
+++ pyCrossyRoad/start.py
@@ -1,6 +1,7 @@
-from turtle import bgpic, Screen, Turtle
+from turtle import bgpic, Screen
 from player import Player, Goal
 from car import Car
+from score_board import Score, Level, Information
 from time import sleep
 
 # Screen configuration
@@ -9,10 +10,17 @@ bgpic('crossyRoadMap.gif')
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
+# Score Board
+score_player = Score()
+level_player = Level()
+info = Information()
+
 # Goal
 goal = Goal()
+
 # Player
 player = Player()
+
 # Car
 car = Car()
 
@@ -30,12 +38,27 @@ screen.onkeypress(fun=player.move_right, key='Right')
 in_game = True
 while in_game:
     screen.update()
-    sleep(0.01)
+    sleep(0.1)
 
+    car.create_car()
     car.move()
 
+    # Calculates the score
+    if player.ycor() == -115 or player.ycor() == 110:
+        score_player.increase_score()
+
+    # Check if you have reached the goal.
     if goal.distance(player) < 22:
-        player.winner()
-        in_game = False
+        score_player.increase_score()
+        level_player.increase_level()
+        player.return_position()
+        car.level_up()
+        info.winner()
+
+    # Detect collision with a car
+    for _car in car.all_cars:
+        if _car.distance(player) < 25:
+            info.game_over()
+            in_game = False
 
 screen.exitonclick()
