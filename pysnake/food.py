fa3daefffc711bbddb8d66ce7a6bee219440b3fb
Danielns <danielnoronha.sh@gmail.com>
2021-02-11 04:18:51 -0300

    add apple image

--- pysnake/food.py
+++ pysnake/food.py
@@ -1,12 +1,14 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 from random import randint
+
+Screen().register_shape('apple.gif')
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.shape('circle')
+        self.shape('apple.gif')
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color('red')
